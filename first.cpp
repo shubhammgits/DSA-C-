69fd18f30597509dab6f8e66ece31378eed1a71d
shubhammgits <shubhamm18.work@gmail.com>
2025-07-17 14:17:05 +0530

    first

---
+++ first.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int age = 25;
+    cout<< "Hello World!" << " I am Shubham"<<endl << "My age is " << age <<endl;
+
+    cout<< sizeof(int) << " bytes" << endl;
+
+    char grade = 'A';
+    cout<< "My grade is " << grade <<endl;
+
+    float gpa = 8.75f;
+    cout<< "My GPA is " << gpa <<endl;
+    
+    bool isStudent = true;
+    double price = 100.99; 
+    
+    return 0;
+}
