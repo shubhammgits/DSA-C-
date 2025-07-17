31665aa797dfaea71a8e34f3ecdd5803ccbc0105
shubhammgits <shubhamm18.work@gmail.com>
2025-07-17 16:20:22 +0530

    first

--- first.cpp
+++ first.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main(){
 
+
+    //BASICS
+
     // int age = 25;
     // cout<< "Hello World!" << " I am Shubham"<<endl << "My age is " << age <<endl;
 
@@ -18,12 +21,25 @@ int main(){
     // double price = 100.99; 
 
 
+
+
+
+
+    //TYPE CONVERSION
+    // Implicit Type Conversion
     char letter = 'S';
     int IntLetter = letter;
     cout<< "The ASCII value of " << letter << " is " << IntLetter << endl;
 
+    // Explicit Type Conversion
     double num = 100.9999;
     int newNum = (int)num;
     cout<< "The integer value of " << num << " is " << newNum <<endl;
+
+
+
+
+    //INPUT in C++
+
     return 0;
 }
