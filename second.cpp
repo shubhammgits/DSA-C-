c857bfbc57376b0ce3b5bc3b34d246da82d49453
shubhammgits <shubhamm18.work@gamil.com>
2025-08-05 17:45:59 +0530

    update

--- second.cpp
+++ second.cpp
@@ -2,17 +2,28 @@
 using namespace std;
 
 int main(){
-    int age;
-    cout<< "Enter your age : "<<endl;
-    cin>>age;
+    // int age;
+    // cout<< "Enter your age : "<<endl;
+    // cin>>age;
 
-    if(age >= 18){
-        cout<<"You can vote!\n";
+    // if(age >= 18){
+    //     cout<<"You can vote!\n";
+    // }
+
+    // else{
+    //     cout<<"You cannot vote!\n";
+    // };
+
+
+    int num;
+    cout<< "Enter any number : ";
+    cin>>num;
+
+    if(num%2 == 0){
+        cout<<"It is EVEN!";
     }
-
     else{
-        cout<<"You cannot vote!\n";
-    }
-
+        cout<<"It is ODD!";
+    };
     return 0;
 }
