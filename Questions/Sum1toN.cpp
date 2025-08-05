8ecc6228587d043e2c114dd5e39626284d176100
shubhammgits <shubhamm18.work@gmail.com>
2025-08-05 21:30:52 +0530

    update

---
+++ Questions/Sum1toN.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sum=0;
+    int n;
+    cout<<"Enter any number :";
+    cin>>n;
+    for(int i=1 ; i<=n ; i++){
+        sum+=i;
+    }
+    cout<<"The sum of numbers from 1 to "<<n<<" is : "<<sum<<endl;
+    return 0;
+}
