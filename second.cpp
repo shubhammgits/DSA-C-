5361f87dd91b9acc20ee2534a507c75685a825b1
shubhammgits <shubhamm18.work@gmail.com>
2025-08-05 21:02:18 +0530

    update

--- second.cpp
+++ second.cpp
@@ -15,41 +15,48 @@ int main(){
     // };
 
 
-    int num;
-    cout<< "Enter any number : ";
-    cin>>num;
+    // int num;
+    // cout<< "Enter any number : ";
+    // cin>>num;
 
-    if(num%2 == 0){
-        cout<<"It is EVEN!";
-    }
-    else{
-        cout<<"It is ODD!";
-    }
+    // if(num%2 == 0){
+    //     cout<<"It is EVEN!";
+    // }
+    // else{
+    //     cout<<"It is ODD!";
+    // }
 
-    return 0;
-
-
-
-    int marks;
-    cout<<"Enter your marks :";
-    cin>> marks;
-
-    if(marks>=90){
-        cout<<"Grade A";
-    }
-
-    else if(marks>=80 && marks<90){
-        cout<<"Grade B";
-    }
-
-    else if(marks>=70 && marks<80){
-        cout<<"Grade C";
-    }
-
-    else{
-        cout<<"Fail";
-    }
+    // return 0;
 
 
-    
+
+    // int marks;
+    // cout<<"Enter your marks :";
+    // cin>> marks;
+
+    // if(marks>=90){
+    //     cout<<"Grade A";
+    // }
+
+    // else if(marks>=80 && marks<90){
+    //     cout<<"Grade B";
+    // }
+
+    // else if(marks>=70 && marks<80){
+    //     cout<<"Grade C";
+    // }
+
+    // else{
+    //     cout<<"Fail";
+    // }
+
+
+
+
+    // Ternary Operators:
+
+    // int n;
+    // cout<< (n>=0 ? "positive" : "negative");
+
+
 }
