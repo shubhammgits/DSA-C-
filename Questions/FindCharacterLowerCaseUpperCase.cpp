b7a3fb7c74f9a09a606ce3debaa33cda738375b2
shubhammgits <shubhamm18.work@gmail.com>
2025-08-05 20:56:19 +0530

    update

---
+++ Questions/FindCharacterLowerCaseUpperCase.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    cout<<"Enter any character :";
+    cin>>ch;
+
+    int ascii = ch;
+    // cout<<ascii <<endl;
+
+    if(65 <= ascii && ascii <= 90){
+        cout<<"Uppercase Character";
+    }
+
+    else if(97 <= ascii && ascii <= 122){
+        cout<<"Lowercase character";
+    }
+
+    else{
+        cout<<"Not alphabetical character";
+    }
+}
