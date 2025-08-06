cdce241e12d0fbc9759a92939f6621eda83f5713
shubhammgits <shubhamm18.work@gmail.com>
2025-08-06 22:07:37 +0530

    update

--- Questions/PrimeOrNot.cpp
+++ Questions/PrimeOrNot.cpp
@@ -6,13 +6,13 @@ int main(){
     cout<<"Enter the number :";
     cin>>num;
 
-    for(int i=1 ; i<=num ; i++){
-        if(num%1==0 && num%i==0){
-            cout<<num<<" is a prime number!";
+    for(int i=2 ; i<=num-1 ; i++){
+        if(num%i==0){
+            cout<<num<<" is not a prime number!";
         }
 
         else{
-            cout<<num<<" is not a prime number!";
+            cout<<num<<" is a prime number!";
         }
     }
     return 0;
