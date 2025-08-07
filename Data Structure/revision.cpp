3ee01d799ef4d0a3ec246ac242aa47b8f1481b5f
shubhammgits <shubhamm18.work@gmail.com>
2025-08-07 22:13:24 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -1,18 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int linearSrch(int arr[], int target, int sz){
-    for(int i=0; i<sz; i++){
-        if(arr[i]==target){
-            cout<<"Target is at "<<i<<" index";
-        }
-    }
-}
-
-int main(){
-    int arr[] = {3,2,5,6,4,7,2,8,9};
-    int sz = sizeof(arr)/sizeof(int);
-    int target = 7;
-
-    cout<<linearSrch(arr, target, sz);
+int reverseArray(int arr[], int n){
+    int start=0, end=n-1;
+    
 }
