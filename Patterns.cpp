31770014172e4021200c9cd5a957386bbaf22ff7
shubhammgits <shubhamm18.work@gmail.com>
2025-08-08 23:45:42 +0530

    update

--- Patterns.cpp
+++ Patterns.cpp
@@ -1,20 +1,58 @@
-//        1 2 3 4
-//        1 2 3 4
-//        1 2 3 4
-//        1 2 3 4
-
-
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    for(int i=1 ; i<=4 ; i++){
-        for(int j=1 ; j<=4 ; j++){
-            cout<<j<<" ";
+
+
+
+    /*
+          1 2 3 4
+          1 2 3 4
+          1 2 3 4
+          1 2 3 4 
+    */       
+    
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+
+
+
+
+
+    /*
+        A B C D
+        A B C D
+        A B C D
+        A B C D
+    */
+
+
+    for(int i=0; i<4 ; i++){
+        char ch='A';
+        for(int j=0; j<4; j++){
+            cout<<ch;
+            ch = ch + 1;
         }
         cout<<endl;
     }
-    
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
