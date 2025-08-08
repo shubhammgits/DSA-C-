17af9bf579c0fd1e347b321a7a5fafd43f9ffa77
shubhammgits <shubhamm18.work@gmail.com>
2025-08-08 23:12:18 +0530

    update

--- Homework/Lec03.cpp
+++ Homework/Lec03.cpp
@@ -1,20 +1,20 @@
 // Sum of all numbers from 1 to N which are divisible by 3
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int main(){
+int main(){
 
-//     int sum=0;
-//     int n=10;
-//     for(int i=1 ; i<=n ; i++){
-//         if(i%3==0){
-//             sum+=i;
-//         }
-//     }
-//     cout<<sum;
-//     return 0;
-// }
+    int sum=0;
+    int n=10;
+    for(int i=1 ; i<=n ; i++){
+        if(i%3==0){
+            sum+=i;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
 
 
 
