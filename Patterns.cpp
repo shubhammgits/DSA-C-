ea668fa786722225a938c6730becb71ce58c64de
shubhammgits <shubhamm18.work@gmail.com>
2025-08-08 23:46:13 +0530

    update

--- Patterns.cpp
+++ Patterns.cpp
@@ -39,7 +39,7 @@ int main()
     for(int i=0; i<4 ; i++){
         char ch='A';
         for(int j=0; j<4; j++){
-            cout<<ch;
+            cout<<ch<<" ";
             ch = ch + 1;
         }
         cout<<endl;
