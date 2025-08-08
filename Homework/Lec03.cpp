ff18c8e7c3bca15c198127115441aac078027279
shubhammgits <shubhamm18.work@gmail.com>
2025-08-08 23:11:31 +0530

    update

--- Homework/Lec03.cpp
+++ Homework/Lec03.cpp
@@ -1,17 +1,37 @@
 // Sum of all numbers from 1 to N which are divisible by 3
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+
+//     int sum=0;
+//     int n=10;
+//     for(int i=1 ; i<=n ; i++){
+//         if(i%3==0){
+//             sum+=i;
+//         }
+//     }
+//     cout<<sum;
+//     return 0;
+// }
+
+
+
+
+// Print factorial of a number N
+
 #include<iostream>
 using namespace std;
 
 int main(){
-
-    int sum=0;
-    int n=10;
+    int fact=1;
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
     for(int i=1 ; i<=n ; i++){
-        if(i%3==0){
-            sum+=i;
-        }
+        fact*=i;
     }
-    cout<<sum;
+    cout<<"The factorial of "<<n<<" is :"<<fact;
     return 0;
 }
