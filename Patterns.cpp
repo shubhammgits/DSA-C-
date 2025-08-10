9cc310ec4c9b1bc5f0a562fcedefc5c5b71553e3
shubhammgits <shubhamm18.work@gmail.com>
2025-08-10 22:09:52 +0530

    update

--- Patterns.cpp
+++ Patterns.cpp
@@ -104,6 +104,30 @@ int main()
 
 
 
+    /*
+       *
+       * *
+       * * *
+       * * * *
+    */
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<i+1; j++){
+            cout<<"*"<<"  ";
+        }
+        cout<<endl;
+    }
+
+
+    
+
+
+
+
+
+
+
+
 
     return 0;
 }
