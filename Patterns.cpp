74baede344e35b2ef358e2c870478def46c93c5a
shubhammgits <shubhamm18.work@gmail.com>
2025-08-10 20:30:57 +0530

    update

--- Patterns.cpp
+++ Patterns.cpp
@@ -60,11 +60,12 @@ int main()
 
     int num=1;
     for(int i=0; i<3; i++){
-        for(int j=1; j<=num; j++){
+        for(int j=0; j<3; j++){
             cout<<num<<" ";
             num++;
         }
         cout<<endl;
+        num++;
     }
 
 
