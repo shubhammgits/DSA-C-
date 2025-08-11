5a80aef370ff5f9cfae77655f5305d9c62712776
shubhammgits <shubhamm18.work@gmail.com>
2025-08-12 01:18:35 +0530

    update

--- Functions.cpp
+++ Functions.cpp
@@ -2,19 +2,41 @@
 using namespace std;
 
 
-int printHello(){
-    cout<<"hello\n";
-    return 3;
+// int printHello(){
+//     cout<<"hello\n";
+//     return 3;
+// }
+
+
+
+int sum(int a, int b){
+    int s = a + b;
+    return s;
 }
+
+
+
+
 
 int main(){
 
-    printHello();
-    printHello();
-    printHello();
+    // printHello();
+    // printHello();
+    // printHello();
 
-    int val = printHello();
-    cout<<val;
-    
+    // int val = printHello();
+    // cout<<"val ="<<val;               
+
+
+    // cout<<printHello();
+
+
+
+
+
+    cout<<sum(2,3)<<endl;
+    cout<<sum(5,8)<<endl;
+
+
     return 0;
 }
