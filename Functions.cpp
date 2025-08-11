cb2dd551c519727a71bec6db48f573ca1951b8e3
shubhammgits <shubhamm18.work@gmail.com>
2025-08-12 01:07:43 +0530

    update

---
+++ Functions.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+void printHello(){
+    cout<<"hello/n";
+}
+
+int main(){
+
+    printHello();
+    printHello();
+    printHello();
+    
+    return 0;
+}
