03f014a8a79db59ed989c85cd3ab51012e9cf465
shubhammgits <shubhamm18.work@gmail.com>
2025-08-12 01:22:34 +0530

    update

--- Functions.cpp
+++ Functions.cpp
@@ -9,9 +9,30 @@ using namespace std;
 
 
 
-int sum(int a, int b){
-    int s = a + b;
-    return s;
+// int sum(int a, int b){
+//     int s = a + b;
+//     return s;
+// }
+
+
+
+
+
+
+
+int minOfTwo(int a, int b){
+    cout<<"Enter the first number :";
+    cin>>a;
+
+    cout<<"Enter the second number :";
+    cin>>b;
+
+    if(a>b){
+        cout<<"Minimum of "<<a<<" and "<<b<<" is :"<<b;
+    }
+    else{
+        cout<<"Minimum of "<<a<<" and "<<b<<" is :"<<a;
+    }
 }
 
 
@@ -34,8 +55,13 @@ int main(){
 
 
 
-    cout<<sum(2,3)<<endl;
-    cout<<sum(5,8)<<endl;
+    // cout<<sum(2,3)<<endl;
+    // cout<<sum(5,8)<<endl;
+
+
+
+
+    cout<<minOfTwo(5,10);
 
 
     return 0;
