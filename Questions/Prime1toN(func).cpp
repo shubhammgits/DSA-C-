f71ae7a1b10530f25d048c9429cf345c47100a23
shubhammgits <shubhamm18.work@gmail.com>
2025-08-14 16:41:11 +0530

    update

---
+++ Questions/Prime1toN(func).cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+bool isPrime(int num){
+    for(int i=2; i*i<=num; i++){
+        if(num%i==0){
+        return false;
+        }  
+    }
+
+    return true;
+}
+
+
+int main(){
+    int n=20;
+
+    for(int i=2; i<=n; i++){
+        if(isPrime(i)){
+            cout<<i<<" ";
+        }
+    }
+    cout<<endl;
+    return 0;
+
+}
