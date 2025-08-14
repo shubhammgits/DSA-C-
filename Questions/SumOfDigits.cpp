165321a70ce5d2a9a2c2efd4793581b7dee7d228
shubhammgits <shubhamm18.work@gmail.com>
2025-08-14 12:54:19 +0530

    update

--- Questions/SumOfDigits.cpp
+++ Questions/SumOfDigits.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 
 
-
-int main(){
-
+int SumOfDigits(int num){
+    
     int digSum=0;
-
-    int num=12345;
 
     while(num>0){
         int lastDig = num%10;
-        
         num = num/10;
 
         digSum += lastDig;
     }
 
-    cout<<digSum<<endl;
+    return digSum;
+}
+
+
+
+
+int main(){
+
+    cout<<"Sum = "<<SumOfDigits(12345)<<endl;
     return 0;
 }
