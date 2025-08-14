d06aaf98b7bad37d3f33bcfc05da4ff925d8bf48
shubhammgits <shubhamm18.work@gmail.com>
2025-08-14 12:21:28 +0530

    update

--- Functions.cpp
+++ Functions.cpp
@@ -20,36 +20,45 @@ using namespace std;
 
 
 
-void minOfTwo(int a, int b){
+// void minOfTwo(int a, int b){
 
-    if(a>b){
-        cout<<"Minimum of "<<a<<" and "<<b<<" is :"<<b;
-    }
-    else{
-        cout<<"Minimum of "<<a<<" and "<<b<<" is :"<<a;
-    }
+//     if(a>b){
+//         cout<<"Minimum of "<<a<<" and "<<b<<" is :"<<b;
+//     }
+//     else{
+//         cout<<"Minimum of "<<a<<" and "<<b<<" is :"<<a;
+//     }
 
-    cout<<endl;
+//     cout<<endl;
     
+// }
+
+
+
+
+
+
+
+
+
+// void fun(){
+//     int x = 25;
+//     cout<<"x = "<<x<<endl;
+// }
+
+
+
+
+
+
+
+int sum(int a, int b){
+    a = a + 10;
+    b = b + 15;
+    return a+b;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-void fun(){
-    int x = 25;
-    cout<<"x = "<<x<<endl;
-}
 
 
 
@@ -84,10 +93,22 @@ int main(){
 
 
 
-    fun();
+    // fun();
     // cout<<x<<endl;         It will return error because x isdefined within the fun() function!
 
 
+
+
+
+
+
+    int a = 5;
+    int b = 10;
+
+    cout<<sum(a,b)<<endl;
+
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
 
 
 
