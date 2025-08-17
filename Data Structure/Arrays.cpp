84a4b7ec4d7560addba76de0f056f80541ec809f
shubhammgits <shubhamm18.work@gmail.com>
2025-08-17 21:58:55 +0530

    update

--- Data Structure/Arrays.cpp
+++ Data Structure/Arrays.cpp
@@ -11,11 +11,26 @@ int main(){
 
     // marks[0] = 100;
 
-    int sz= sizeof(marks)/sizeof(int);
+    // int sz= sizeof(marks)/sizeof(int);
 
-    for(int i =0; i<sz; i++){
-        cout<<marks[i]<<endl;
+    // for(int i =0; i<sz; i++){
+    //     cout<<marks[i]<<endl;
+    // }
+
+
+
+
+    int price[5];
+    int sz = 5;
+
+    for(int i=0; i<sz; i++){
+        cout<<"Enter the element:";
+        cin>>marks[i];
     }
+
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
 
     return 0;
 }
