be4507f7fada1a9141f8eb0fc44fea1284b87a9f
shubhammgits <shubhamm18.work@gmail.com>
2025-08-18 21:37:55 +0530

    update

--- Data Structure/Questions/SmallestLargestInArray.cpp
+++ Data Structure/Questions/SmallestLargestInArray.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main(){
-    int num[] = {2,4,5,2,35,7,9,10,8};
+    int num[] = {2,4,5,2,-5,7,-15,8,1};
 
-    int smallest = num[0];
+    int smallest = INT8_MAX;
 
     int sz = sizeof(num)/sizeof(int);
     for(int i=0; i<sz; i++){
