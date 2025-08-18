b54467e25f7394cdfd24e288357b8e1449bbab00
shubhammgits <shubhamm18.work@gmail.com>
2025-08-18 22:03:54 +0530

    update

--- Data Structure/Questions/SmallestLargestInArray.cpp
+++ Data Structure/Questions/SmallestLargestInArray.cpp
@@ -7,19 +7,22 @@ int main(){
     // int smallest = INT8_MAX;
 
     int smallest = num[0];
+    int index = -1;
 
     int sz = sizeof(num)/sizeof(int);
     for(int i=0; i<sz; i++){
 
-        // if(num[i] < smallest){
-        //     smallest = num[i];
-        // }
+        if(num[i] < smallest){
+            smallest = num[i];
+            index = i;
+        }
 
 
-        smallest = min(num[i], smallest);
+        // smallest = min(num[i], smallest);
         
     }
-
+    cout<<"Index = "<<index<<endl;
     cout<<smallest;
+    
 
 }
