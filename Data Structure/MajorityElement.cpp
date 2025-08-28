f027954e77987baf4c3543838f17461506d4e0ff
shubhammgits <shubhamm18.work@gmail.com>
2025-08-28 23:10:27 +0530

    6/09/25

--- Data Structure/MajorityElement.cpp
+++ Data Structure/MajorityElement.cpp
@@ -29,3 +29,28 @@ using namespace std;
 //     vector<int> nums = {2,2,1,1,1,2,2};
 //     cout<<majorityElement(nums);
 // }
+
+
+
+
+
+
+
+
+// Brute Force (Optimisez - Sorting)
+
+
+// int majorityElement(vector<int> nums){
+//     int n = nums.size();
+//     sort(nums.begin(), nums.end());
+
+//     .....
+// }
+
+
+
+
+
+
+
+
