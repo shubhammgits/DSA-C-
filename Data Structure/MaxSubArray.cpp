f133e2d7a0242613d5360018efb56ee8f428da4e
shubhammgits <shubhamm18.work@gmail.com>
2025-08-30 10:46:11 +0530

    30aug

--- Data Structure/MaxSubArray.cpp
+++ Data Structure/MaxSubArray.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int main(){
 
-    int arr[5] = {1,2,3,4,5};
-    int n=5;
 
-    for(int st=0; st<n; st++){
-        for(int end=st; end<n; end++){
-            for(int i=st; i<=end; i++){
-                cout<<arr[i];
-            }
-            cout<<" ";
-        }
-        cout<<endl;
-    }
-}
+
+
+// To print all the subarrays:
+
+
+// int main(){
+
+//     int arr[5] = {1,2,3,4,5};
+//     int n=5;
+
+//     for(int st=0; st<n; st++){
+//         for(int end=st; end<n; end++){
+//             for(int i=st; i<=end; i++){
+//                 cout<<arr[i];
+//             }
+//             cout<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+// To print the sum of all the subarrays (Brute force approach):
