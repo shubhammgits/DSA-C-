c203f90b0d973f49d01e8108090389fa1d8bff4f
shubhammgits <shubhamm18.work@gmail.com>
2025-08-30 23:28:40 +0530

    add

--- Data Structure/PairSum.cpp
+++ Data Structure/PairSum.cpp
@@ -81,5 +81,10 @@ vector<int> pairSum(vector<int> nums, int target){
 
 
 int main(){
-    
+    vector<int> nums = {2,7,11,13};
+    int target = 13;
+
+    vector<int> ans = pairSum(nums, target);
+    cout<< ans[0] << " , "<< ans[1]<<endl;
+    return 0;
 }
