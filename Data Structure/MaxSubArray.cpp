0da028aca518eb4dec4ad0862273ad9e392a6acf
shubhammgits <shubhamm18.work@gmail.com>
2025-08-30 10:50:06 +0530

    30aug

--- Data Structure/MaxSubArray.cpp
+++ Data Structure/MaxSubArray.cpp
@@ -34,3 +34,19 @@ using namespace std;
 
 
 // To print the sum of all the subarrays (Brute force approach):
+
+
+int main(){
+    int arr[] = {3, -4, 5, 4, -1, 7, -8};
+    int n = 7;
+
+    int maxSum;
+    for(int st=0; st<n; st++){
+        int currSum = 0;
+        for(int end=st; end<n; end++){
+
+            currSum += arr[end];
+            maxSum = max(currSum, maxSum);
+        }
+    }
+}
