f7a8f580b81b27b59d10af29f038fc9bbcba446f
shubhammgits <shubhamm18.work@gmail.com>
2025-09-04 21:19:01 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 int main(){
 
+    char ch='A';
     for(int i=1; i<=4; i++){
         for(int j=0; j<i; j++){
-            cout<<"*"<<" ";
+            cout<<ch<<" ";
         }
+        ch++;
         cout<<endl;
     }
 }
