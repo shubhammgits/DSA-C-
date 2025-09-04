19f2c2090f9cc3cec8fa5fbbdb83a31a8ca3284d
shubhammgits <shubhamm18.work@gmail.com>
2025-09-04 22:03:16 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -1,16 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int changeX(int x){
-    x = 2*x;
-    cout<<"Value in fucntion : ";
-    return x;
+int changeX(int arr[], int n){
+    for(int i=0; i<n; i++){
+        arr[i] = 2*arr[i];
+    }
+
+    cout<<"Array in changeX()"<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+    }
 }
 
 int main(){
-    int x = 10;
-    changeX(x);
+    int arr[] = {1,2,3,4,5};
 
-    cout<<"Value in main:";
-    cout<<x;
+    cout<<changeX(arr, 5)<<endl;
+    cout<<"Array in main():"<<endl;
+    for(int i=0; i<5; i++){
+        cout<<arr[i];
+    }
 }
