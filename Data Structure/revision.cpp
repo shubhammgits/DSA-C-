c1416f428066a3dc159b485987de12be520c4c25
shubhammgits <shubhamm18.work@gmail.com>
2025-09-04 20:33:31 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int sum1toN(int n){
     int sum = 0;
-    for(int i=0; i<=n; i++){
-        if(n%2 != 0){
+    for(int i=1; i<n; i++){
+        if(i%2 != 0){
             sum += n;
         }
     }
@@ -12,6 +12,6 @@ int sum1toN(int n){
 }
 
 int main(){
-    int n = 10;
+    int n = 5;
     cout<<"Sum ="<<sum1toN(n);
 }
