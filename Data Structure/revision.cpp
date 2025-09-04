74896638429a30e3900c49c9dbd78f3b1af96c25
shubhammgits <shubhamm18.work@gmail.com>
2025-09-04 20:44:14 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -2,16 +2,13 @@
 using namespace std;
 
 int main(){
-    int num = 7;
-    for(int i=2; i*i<num; i++){
-        if(num%i == 0){
-            cout<<"Its not prime!";
-            break;
-        }
 
-        else{
-            cout<<"Its a prime number!";
-            break;
+    int num=1;
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            cout<<num<<" ";
+            num++;
         }
+        cout<<endl;
     }
 }
