40ff7e0f5e4f52a44fe738ac61785c50072bea20
shubhammgits <shubhamm18.work@gmail.com>
2025-09-04 21:53:32 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -2,13 +2,18 @@
 using namespace std;
 
 int main(){
+    int nums[] = {4,6,8,3,1,2,6,8,9};
+    int n = sizeof(nums)/sizeof(int);
 
-    char ch='A';
-    for(int i=1; i<=4; i++){
-        for(int j=0; j<i; j++){
-            cout<<ch<<" ";
+    int smallest = nums[0];
+    int index = 0;
+    for(int i=0; i<n; i++){
+        if(nums[i] < smallest){
+            smallest = nums[i];
+            index = i;
         }
-        ch++;
-        cout<<endl;
     }
+    
+
+    cout<<smallest;
 }
