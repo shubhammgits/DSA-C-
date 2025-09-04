c4cc4c4a03c9c59ac72b45f82ae0c108b89b9f53
shubhammgits <shubhamm18.work@gmail.com>
2025-09-04 20:46:57 +0530

    revision

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -3,11 +3,9 @@ using namespace std;
 
 int main(){
 
-    int num=1;
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            cout<<num<<" ";
-            num++;
+    for(int i=1; i<=4; i++){
+        for(int j=0; j<i; j++){
+            cout<<"*"<<" ";
         }
         cout<<endl;
     }
