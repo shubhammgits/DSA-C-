abd4b804bf0ad92f4cbb8c8cc52072113759ee4e
shubhammgits <shubhamm18.work@gmail.com>
2025-09-06 01:15:15 +0530

    5/09/25

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -2,23 +2,26 @@
 #include<vector>
 using namespace std;
 
-vector<int> pairSum(vector<int> nums, int target){
-    int sz = nums.size();
-    vector<int> ans;
-    for(int i=0; i<sz; i++){
-        for(int j=i+1; j<sz; j++){
-            if(nums[i] + nums[j] == target){
-                ans.push_back(nums[i]);
-                ans.push_back(nums[j]);
-            }
+int pairSum(int arr[], int sz, int target){
+    int start=0, end=sz-1;
+
+    while(start<end){
+        int ps = arr[start]+arr[end];
+        if(ps<target){
+            start++;
+        }
+        else if(ps>target){
+            end--;
+        }
+
+        else{
+            return ps;
         }
     }
-    return ans;
 }
 
 int main(){
-    vector<int> nums = {2,7,11,15};
-    int target = 13;
-    vector<int> ans = pairSum(nums, target);
-    cout<<ans[0]<<ans[1];
+    int arr[] = {2,7,11,15};
+    int n = 4, target = 13;
+    cout<<pairSum(arr, n, target);
 }
