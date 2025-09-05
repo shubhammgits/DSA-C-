5be0e80819c2823117bb5ed9da3ff16e68492a88
shubhammgits <shubhamm18.work@gmail.com>
2025-09-05 23:54:47 +0530

    5/09/25

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main(){
-    int arr[] = {1,2,3,4,5,6};
-    int sz = 6;
+    int arr[] = {3,-4,5,4,-1,7,-8};
+    int sz = 7;
 
+    int maxSum = 0;
     for(int start=0; start<sz; start++){
+        int currSum=0;
         for(int end=start; end<sz; end++){
-            for(int i=start; i<=end; i++){
-                cout<<arr[i];
-            }
-            cout<<" ";
+            currSum = currSum + arr[end];
+            maxSum = max(currSum, maxSum);
         }
-        cout<<endl;
     }
+    cout<<maxSum;
 }
