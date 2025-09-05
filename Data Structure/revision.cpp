cc03a60e6b424587bce4d278070728f0cf149cac
shubhammgits <shubhamm18.work@gmail.com>
2025-09-05 23:42:43 +0530

    5/09/25

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -3,12 +3,13 @@
 using namespace std;
 
 int main(){
-    vector<int> nums = {1,2,3,4,5,6};
+    int arr[] = {1,2,3,4,5,6};
     int sz = 6;
-    for(int startInd=0; startInd<sz; startInd++){
-        for(int endInd=startInd; endInd<sz; endInd++){
-            for(int val: nums){
-                cout<<val<<" ";
+
+    for(int start=0; start<sz; start++){
+        for(int end=start; end<sz; end++){
+            for(int i=start; i<end; i++){
+                cout<<arr[i]<<" ";
             }
             cout<<endl;
         }
