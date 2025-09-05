22447a18c7cf6127f8c6dd99b6bbaf36180e24b1
shubhammgits <shubhamm18.work@gmail.com>
2025-09-05 23:23:40 +0530

    5/09/25

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -1,23 +1,15 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-void reverseArray(int arr[], int n){
-    int start=0, end=n-1;
-    while(start<end){
-        swap(arr[start], arr[end]);
-        start++;
-        end--;
-    }
-
-}
-
 int main(){
-    int arr[] = {1,2,3,4,5};
-    int n = 5;
-
-    reverseArray(arr, n);
-
-    for(int i=0; i<n; i++){
-        cout<<arr[i];
+    vector<int> nums = {1,2,3,4,5};
+    cout<<nums.size()<<endl;
+    
+    for(int val : nums){
+        cout<<val<<" ";
     }
+    cout<<endl;
+
+    
 }
