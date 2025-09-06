29b4372b10d93bf48a18009a5b56d248aefd3c64
shubhammgits <shubhamm18.work@gmail.com>
2025-09-06 22:42:57 +0530

    6/09/25

--- Data Structure/MajorityElement.cpp
+++ Data Structure/MajorityElement.cpp
@@ -2,25 +2,30 @@
 #include<vector>
 using namespace std;
 
-int majorityElement(vector<int> nums){
-    int n = nums.size();
-    int freq = 0;
-    for(int val : nums){
-        for(int elem : nums){
-            if(elem == val){
-                freq++;
-            }
-        }
 
-        if(freq > n/2){
-            return val;
-        }
-    }
+// Brute Force Approach:
 
-    return -1;
-}
 
-int main(){
-    vector<int> nums = {2,2,1,1,1,2,2};
-    cout<<majorityElement(nums);
-}
+
+// int majorityElement(vector<int> nums){
+//     int n = nums.size();
+//     int freq = 0;
+//     for(int val : nums){
+//         for(int elem : nums){
+//             if(elem == val){
+//                 freq++;
+//             }
+//         }
+
+//         if(freq > n/2){
+//             return val;
+//         }
+//     }
+
+//     return -1;
+// }
+
+// int main(){
+//     vector<int> nums = {2,2,1,1,1,2,2};
+//     cout<<majorityElement(nums);
+// }
