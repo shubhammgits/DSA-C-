aaaca53fde1ba21b330e1d4207171b8fec4f6270
shubhammgits <shubhamm18.work@gmail.com>
2025-09-07 23:15:01 +0530

    7/09/25

--- Data Structure/Pow(x,n).cpp
+++ Data Structure/Pow(x,n).cpp
@@ -2,5 +2,15 @@
 using namespace std;
 
 double myPow(double x, int n){
-    
+    if(n==0) return 1.0;
+    if(x==0) return 0.0;
+    if(x == -1 && n%2 == 0) return 1.0;
+    if(x == -1 && n%2 != 0) return -1.0;
+
+    long binForm = n;
+
+    if(n<0){
+        x=1/x;
+        binForm = -binForm;
+    }
 }
