ccbb4c051427b4705fd910ca02c5458fe1cfa408
shubhammgits <shubhamm18.work@gmail.com>
2025-09-09 23:25:31 +0530

    9/09/25

--- Data Structure/Pointers.cpp
+++ Data Structure/Pointers.cpp
@@ -7,4 +7,18 @@ int main(){
 
     int* ptr = &a;
     cout<<ptr<<endl;
+
+    cout<<&ptr<<endl;
+
+
+
+
+
+    char ch = 'S';
+    cout<<&ch<<endl;
+
+    char* ptr2 = &ch;
+    cout<<ptr2<<endl;
+
+    cout<<&ptr2<<endl;
 }
