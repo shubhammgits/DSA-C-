a7bce1285df512d221f7fe984d58c20fe619c1de
shubhammgits <shubhamm18.work@gmail.com>
2025-09-09 20:27:37 +0530

    9/09/25

--- Data Structure/ProductOfArrayExceptSelf.cpp
+++ Data Structure/ProductOfArrayExceptSelf.cpp
@@ -2,28 +2,33 @@
 #include<vector>
 using namespace std;
 
-vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n, 1);
+
+
+
+// Brute Force
+
+// vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans(n, 1);
         
-        for(int i=0; i<n; i++){
+//         for(int i=0; i<n; i++){
 
-            for(int j=0; j<n; j++){
-                if(i!=j){
-                    ans[i] *= nums[j];
-                }
-            }
-        }
+//             for(int j=0; j<n; j++){
+//                 if(i!=j){
+//                     ans[i] *= nums[j];
+//                 }
+//             }
+//         }
 
-    return ans;
-}
+//     return ans;
+// }
 
-int main(){
-    vector<int> nums = {1,2,3,4};
-    vector<int> ans = productExceptSelf(nums);
+// int main(){
+//     vector<int> nums = {1,2,3,4};
+//     vector<int> ans = productExceptSelf(nums);
 
-    int n = nums.size();
-    for(int i=0; i<n; i++){
-        cout<<ans[i]<<" ";
-    }
-}
+//     int n = nums.size();
+//     for(int i=0; i<n; i++){
+//         cout<<ans[i]<<" ";
+//     }
+// }
