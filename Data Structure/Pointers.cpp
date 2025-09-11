a8b7b706b153c5e25ad2c884401f0915bbe2632e
shubhammgits <shubhamm18.work@gmail.com>
2025-09-11 22:34:11 +0530

    11/09/25

--- Data Structure/Pointers.cpp
+++ Data Structure/Pointers.cpp
@@ -124,5 +124,19 @@ int main(){
 
     ptr--;
     cout<<ptr<<endl;      //decrement to original address
+
+    ptr += 3;
+    cout<<ptr<<endl;
+
+
+
+
+    int arr[] = {1,2,3,4,5};
+
+    
+    cout<<*arr<<endl;
+    cout<<*(arr+1)<<endl;
+    cout<<*(arr+2)<<endl;
+    cout<<*(arr+3)<<endl;
 }
 
