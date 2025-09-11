3ae18bb8332fdc40cc92f566b383cc4a4c40c924
shubhammgits <shubhamm18.work@gmail.com>
2025-09-11 20:37:31 +0530

    11/09/25

--- Data Structure/Pointers.cpp
+++ Data Structure/Pointers.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 using namespace std;
 
+
+void changeA(int a){
+    a = 20;
+}
+
+
+
+
+
 int main(){
     // int a=10;
 
@@ -58,11 +67,12 @@ int main(){
 
 
 
+
+
+    // Pass By Referance using Pointers:
+
     int a = 10;
     changeA(a);
     cout<<a<<endl;
 }
 
-void changeA(int a){
-    a = 20;
-}
