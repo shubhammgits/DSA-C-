6de6c4adb154089219d7871526739d9e1f8258e6
shubhammgits <shubhamm18.work@gmail.com>
2025-09-12 23:13:34 +0530

    12/09/25

---
+++ Data Structure/BinarySearch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int target){
+    int n = sizeof(arr)/sizeof(int);
+    int start=0, end=n-1;
+    
+    int mid = (start+end)/2;
+    for(int i=start; i<end; i++){
+        if(target > arr[mid]){
+        start = mid + 1;
+    }
+
+    else if(target < arr[mid]){
+        end = mid-1;
+    }
+
+    else {
+        return mid;
+    }
+    }
+}
+
+
+int main(){
+    int arr[] = {-1,0,3,5,7,9,11,13};
+    int target = 13;
+}
