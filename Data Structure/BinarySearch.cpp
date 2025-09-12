5953b34ba4faad210b9c598d9d66134788f5e157
shubhammgits <shubhamm18.work@gmail.com>
2025-09-13 00:50:21 +0530

    12/09/25

--- Data Structure/BinarySearch.cpp
+++ Data Structure/BinarySearch.cpp
@@ -6,7 +6,7 @@ int binarySearch(vector<int> arr, int tar){
     int st=0, end=arr.size() - 1;
 
     while(st<=end){
-        int mid = (st+end)/2;
+        int mid = st + (end-st)/2;
 
         if(tar>arr[mid]) st=mid+1;
 
@@ -21,6 +21,6 @@ int binarySearch(vector<int> arr, int tar){
 
 int main(){
     vector<int> arr= {-1,0,3,5,7,9,11,13};
-    int target = 0;
+    int target = 40;
     cout<<binarySearch(arr, target);
 }
