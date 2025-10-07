8cea7bb89105a1a9240e5b8553135316a9619c15
shubhammgits <shubhamm18.work@gmail.com>
2025-10-07 23:18:26 +0530

    new

--- Data Structure/revision.cpp
+++ Data Structure/revision.cpp
@@ -1,11 +1,19 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
-    vector<int> nums;
-    if(nums.size() == 0){
-        cout<<"0 size";
+    int arr[] = {5,-9,4,10,13,-10,5,4,3};
+    int sz = sizeof(arr)/sizeof(int);
+
+    int smallest = INT8_MAX;
+    int largest = INT8_MIN;
+
+    for(int i=0; i<sz; i++){
+        if(arr[i] > largest) largest = arr[i];
+
+        if(arr[i] < smallest) smallest = arr[i];
     }
-    
+
+    cout<<"Smallest = "<<smallest<<endl;
+    cout<<"Largest ="<<largest<<endl;
 }
