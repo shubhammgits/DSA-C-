246d78762af72a4e1ca1597fca35c0dcdb99c330
Shubham <shubhamm18.work@gmail.com>
2025-10-24 01:49:56 +0530

    update

--- LOOP Questions/4.cpp
+++ LOOP Questions/4.cpp
@@ -3,4 +3,19 @@
 #include<iostream>
 using namespace std;
 
-void main
+void checkPrime(int num){
+    bool isPrime = true;
+    for(int i=2; i*i<= num; i++){
+        if(num%i == 0){
+            isPrime = false;
+            break;
+        }
+    }
+
+    if(isPrime == true) cout<<num<<" is Prime!"<<endl;
+    else cout<<num<<" is not Prime!"<<endl;
+}
+
+int main(){
+    checkPrime(4);   
+}
