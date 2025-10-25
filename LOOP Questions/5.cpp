b861a940b2202aa18c1775f4f872bac99da62319
Shubham <shubhamm18.work@gmail.com>
2025-10-25 22:15:39 +0530

    update

--- LOOP Questions/5.cpp
+++ LOOP Questions/5.cpp
@@ -3,15 +3,22 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    int n = 10;
+void perfectNum(int num){
     int factSum = 0;
-    for(int i=1; i<n; i++){
-        if(n%i ==0){
+    for(int i=1; i<num; i++){
+        if(num%i == 0){
             factSum += i;
         }
     }
 
-    if(factSum == n) cout<<"Perfect Number!";
-    else cout<<"Not a perfect number!";
+    if(factSum == num) cout<<num<<" is Perfect Number!"<<endl;
+    else cout<<num<<" is not a Perfect Number!"<<endl;
+}
+
+int main(){
+    perfectNum(6);
+    perfectNum(49);
+    perfectNum(496);
+    perfectNum(496);
+    perfectNum(4960);
 }
