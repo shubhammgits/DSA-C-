9f6b4745e1372c6f3cd48886c7f9fd44b4d1ae42
Shubham <shubhamm18.work@gmail.com>
2025-10-27 14:10:14 +0530

    update

--- LOOP Questions/2.cpp
+++ LOOP Questions/2.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 int main(){
-    cout<<"Sum pf first 10 natural numbers: ";
+    cout<<"Sum of first 10 natural numbers: ";
     int sum = 0;
     for(int i=1; i<=10; i++){
         sum += i;
     }
-    cout<<"Sum = "<<sum;
+    cout<<sum;
 }
