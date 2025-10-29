8e033ac64890ff73965c9dda221e6357f77dabda
Shubham <shubhamm18.work@gmail.com>
2025-10-29 14:29:03 +0530

    update

--- LOOP Questions/4.cpp
+++ LOOP Questions/4.cpp
@@ -1,10 +1,14 @@
-// Prime or not
+// Prime or not!
 
 #include<iostream>
 using namespace std;
 
 void checkPrime(int num){
     bool isPrime = true;
+    if(num < 2){
+        cout<<num<<" is not a Prime Number!"<<endl;
+        return;
+    }
     for(int i=2; i*i<=num; i++){
         if(num%i == 0){
             isPrime = false;
@@ -12,14 +16,14 @@ void checkPrime(int num){
         }
     }
 
-    if(isPrime == true) cout<<num<<" is a Prime Number!";
-    else cout<<num<<" is not a Prime Number!";
+    if(isPrime == true) cout<<num<<" is a Prime Number!"<<endl;
+    else cout<<num<<" is not a Prime Number!"<<endl;
 }
 
 int main(){
+    checkPrime(0);
+    checkPrime(1);
     checkPrime(3);
     checkPrime(4);
-    checkPrime(5);
-    checkPrime(67);
-    checkPrime(6677);
+    checkPrime(7);
 }
