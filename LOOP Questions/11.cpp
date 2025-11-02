5513b0f09f8f673a866fe2fd1b1b147effc569f4
Shubham <shubhamm18.work@gmail.com>
2025-11-02 18:15:23 +0530

    update

--- LOOP Questions/11.cpp
+++ LOOP Questions/11.cpp
@@ -1,14 +1,15 @@
 // Find smallest and largest from an array:
 
 #include<iostream>
+#include<climits>
 using namespace std;
 
 int main(){
     int arr[] = {3,2,6,7,4,9,2,0};
     int n = sizeof(arr)/sizeof(int);
 
-    int min = INT16_MAX;
-    int max = INT16_MIN;
+    int min = INT_MAX;
+    int max = INT_MIN;
     for(int i=0; i<n; i++){
         if(arr[i] < min){
             min = arr[i];
