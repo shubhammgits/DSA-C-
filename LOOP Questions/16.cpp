1d33310bc3ea702229cda3fcdf64197196ee8a4b
Shubham <shubhamm18.work@gmail.com>
2025-11-02 19:17:57 +0530

    update

---
+++ LOOP Questions/16.cpp
@@ -0,0 +1,30 @@
+// Print all the unique elements in an array
+
+#include<iostream>
+using namespace std;
+
+void uniqueElements(int arr[], int sz){
+
+    for(int i=0; i<sz; i++){
+        bool isUnique = true;
+        for(int j=0; j<sz; j++){
+
+            if(i!=j && arr[i] == arr[j]){
+                isUnique = false;
+                break;
+            }
+        }
+
+        if(isUnique){
+            cout<<arr[i]<<" ";
+        }
+    }
+}
+
+int main(){
+    int arr[] = {1,3,1,4,2};
+    int sz = sizeof(arr)/sizeof(int);
+
+    uniqueElements(arr,sz);
+}
+
