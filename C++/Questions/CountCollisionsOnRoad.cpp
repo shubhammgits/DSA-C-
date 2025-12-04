6783319990a6ad1574a4b7c27b9025e8419f6f49
Shubham <shubhamm18.work@gmail.com>
2025-12-04 20:48:02 +0530

    new question

--- C++/Questions/CountCollisionsOnRoad.cpp
+++ C++/Questions/CountCollisionsOnRoad.cpp
@@ -1,3 +1,18 @@
 #include<iostream>
 using namepsace std;
 
+class Solution {
+public:
+    int countCollisions(string directions) {
+        int n = directions.size();
+        int l = 0;
+        while (l < n && directions[l] == 'L') ++l;
+        int r = n - 1;
+        while (r >= 0 && directions[r] == 'R') --r;
+        if (l > r) return 0;
+        int cnt = 0;
+        for (int i = l; i <= r; ++i) if (directions[i] != 'S') ++cnt;
+        return cnt;
+        }
+
+};
