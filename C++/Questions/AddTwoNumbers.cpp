650ae025c3096d42be84685c1d5095a1accaeed3
Shubham <shubhamm18.work@gmail.com>
2025-12-04 20:46:50 +0530

    new question

--- C++/Questions/AddTwoNumbers.cpp
+++ C++/Questions/AddTwoNumbers.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(NULL) {}
- *     ListNode(int x) : val(x), next(NULL) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-
 #include<iostream>
 using namespace std;
 
