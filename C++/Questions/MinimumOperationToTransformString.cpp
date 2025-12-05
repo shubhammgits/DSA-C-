e2c670ef611ab5214c71567393e83a937b1a690c
Shubham <shubhamm18.work@gmail.com>
2025-12-05 20:53:03 +0530

    new question

--- C++/Questions/MinimumOperationToTransformString.cpp
+++ C++/Questions/MinimumOperationToTransformString.cpp
@@ -1,2 +1,14 @@
 #include<iostream>
 using namespace std;
+
+class Solution {
+public:
+    int minOperations(string s) {
+         int ans = 0;
+        for (char c : s) {
+            int steps = (26 + ('a' - c)) % 26;
+            ans = max(ans, steps);
+        }
+        return ans;
+    }
+};
