2a28c1e283e3ab1ac1559b027914dbbdb4fa1ebc
Shubham <shubhamm18.work@gmail.com>
2025-12-08 23:37:43 +0530

    newquestion

--- C++/Questions/Count square sum triples.cpp
+++ C++/Questions/Count square sum triples.cpp
@@ -1,3 +1,19 @@
 #include<iostream>
 using namespace std;
 
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans = 0;
+        for (int a = 1; a <= n; ++a) {
+            for (int b = 1; b <= n; ++b) {
+                int sum = a * a + b * b;
+                int c = (int)std::sqrt(sum);
+                if (c <= n && c * c == sum) {
+                    ++ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
