eb75e1a02e7b66be068023aa86bc960f369bd1b5
Shubham <shubhamm18.work@gmail.com>
2025-12-10 23:13:41 +0530

    newquestion

--- C++/Questions/MinimumOperationsToEqualizeArray.cpp
+++ C++/Questions/MinimumOperationsToEqualizeArray.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); ++i) {
+        for (int i = 1; i < nums.size(); ++i){
             if (nums[i] != nums[0]) return 1;
         }
         return 0;
