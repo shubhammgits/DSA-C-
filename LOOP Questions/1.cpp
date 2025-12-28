d34a73dafedac8ce995927b6b26092b2203ee4ef
shubhammgits <shubhamm18.work@gmail.com>
2025-12-28 21:34:22 +0530

    revision

--- LOOP Questions/1.cpp
+++ LOOP Questions/1.cpp
@@ -1,12 +1,3 @@
 // First 10 Natural numbers
 
-#include<iostream>
-using namespace std;
 
-
-int main(){
-    cout<<"First 10 natural numbers : ";
-    for(int i=1; i<=10; i++){
-        cout<<i<<" ";
-    }
-}
