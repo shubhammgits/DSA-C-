2bd43fee4a5e65902d700e5715bd2eb63cdabb55
shubhammgits <shubhamm18.work@gmail.com>
2026-06-01 15:30:19 +0530

    revision

--- C++/revision.cpp
+++ C++/revision.cpp
@@ -2,12 +2,32 @@
 using namespace std;
 
 int main(){
-    cout<<"Namaste Duniya"<<'\n';
+    // cout<<"Namaste Duniya"<<'\n';
 
-    int age = 20;
-    char ch = 'S';
-    bool True;
-    float f = 3.14;
-    double d = 1.23456;
+    // int age = 20;
+    // char ch = 'S';
+    // bool bl = true;
+    // float f = 3.14;
+    // double d = 1.23456;
 
+    // cout<<age<<endl;
+    // cout<<ch<<endl;
+    // cout<<bl<<endl;
+    // cout<<f<<endl;
+    // cout<<d<<endl;
+
+    // cout<<"Size of int age : " << sizeof(age) <<endl;
+    // cout<<"Size of char ch : " << sizeof(ch) <<endl;
+    // cout<<"Size of bool bl : " << sizeof(bl) <<endl;
+    // cout<<"Size of float f : " <<sizeof(f) <<endl;
+    // cout<<"Size of double d : " <<sizeof(d) <<endl;
+    
+    int a = 'a';
+    cout<<a<<endl;
+    
+    char ch1 = 98;
+    cout<<ch1<<endl;
+
+    char ch2 = 12345;
+    cout<<ch2<<endl;
 }
