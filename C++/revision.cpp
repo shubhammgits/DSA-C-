c6f0e79e352e716eee200994ddf3ab8f3ba702cb
shubhammgits <shubhamm18.work@gmail.com>
2026-06-01 15:49:23 +0530

    revision

--- C++/revision.cpp
+++ C++/revision.cpp
@@ -2,41 +2,19 @@
 using namespace std;
 
 int main(){
-    // cout<<"Namaste Duniya"<<'\n';
+    // operators:
 
-    // int age = 20;
-    // char ch = 'S';
-    // bool bl = true;
-    // float f = 3.14;
-    // double d = 1.23456;
+    int a = 4;
+    int b = 9;
 
-    // cout<<age<<endl;
-    // cout<<ch<<endl;
-    // cout<<bl<<endl;
-    // cout<<f<<endl;
-    // cout<<d<<endl;
+    cout<<"a+b"<<a+b<<endl;
+    cout<<"a-b"<<a-b<<endl;
+    cout<<"b/a"<<b/a<<endl;
+    cout<<"b%a"<<b%a<<endl;
+    cout<<"b*a"<<b*a<<endl;
 
-    // cout<<"Size of int age : " << sizeof(age) <<endl;
-    // cout<<"Size of char ch : " << sizeof(ch) <<endl;
-    // cout<<"Size of bool bl : " << sizeof(bl) <<endl;
-    // cout<<"Size of float f : " <<sizeof(f) <<endl;
-    // cout<<"Size of double d : " <<sizeof(d) <<endl;
-    
-    int a = 'a';
-    cout<<a<<endl;
-    
-    char ch1 = 98;
-    cout<<ch1<<endl;
+    int c = 5;
+    float d = 9;
 
-    // char ch2 = 12345;
-    // cout<<ch2<<endl;
-
-    int cgpa = (int) 9.12;
-    cout<<cgpa<<endl;
-
-    unsigned int num = 123;
-    cout<<num<<endl;
-
-    unsigned int negnum = -123;
-    cout<<negnum<<endl;
+    cout<<"c/d="<<c/d<<endl;
 }
