f6b733007ab0f91d72b4fe454e01f90ebeaad388
shubhammgits <shubhamm18.work@gmail.com>
2026-06-01 15:18:12 +0530

    revision

--- C++/revision.cpp
+++ C++/revision.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main(){
-    cout<<"Namaste Duniya"<<endl;
+    cout<<"Namaste Duniya"<<'\n';
+
+    int age = 20;
+    char ch = 'S';
+    bool True;
+    float f = 3.14;
+    double d = 1.23456;
+
 }
