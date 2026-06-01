4d8bf5684e0498a080ae9b69166172bbc36f7b5d
shubhammgits <shubhamm18.work@gmail.com>
2026-06-01 15:53:14 +0530

    revision

--- C++/revision.cpp
+++ C++/revision.cpp
@@ -4,17 +4,35 @@ using namespace std;
 int main(){
     // operators:
 
-    int a = 4;
-    int b = 9;
+    // int a = 4;
+    // int b = 9;
 
-    cout<<"a+b"<<a+b<<endl;
-    cout<<"a-b"<<a-b<<endl;
-    cout<<"b/a"<<b/a<<endl;
-    cout<<"b%a"<<b%a<<endl;
-    cout<<"b*a"<<b*a<<endl;
+    // cout<<"a+b"<<a+b<<endl;
+    // cout<<"a-b"<<a-b<<endl;
+    // cout<<"b/a"<<b/a<<endl;
+    // cout<<"b%a"<<b%a<<endl;
+    // cout<<"b*a"<<b*a<<endl;
 
-    int c = 5;
-    float d = 9;
+    // int c = 5;
+    // float d = 9;
 
-    cout<<"c/d="<<c/d<<endl;
+    // cout<<"c/d="<<c/d<<endl;
+
+    int a = 5;
+    int b = 8;
+
+    bool first = a==b;
+    cout<<first<<endl;
+
+    bool second = a!=b;
+    cout<<second<<endl;
+
+    bool third = a>b;
+    cout<<third<<endl;
+
+    bool fourth = a<=b;
+    cout<<fourth<<endl;
+
+    int d = 578;
+    cout<<!a<<endl;
 }
